c7e66dd0c94415df453bec39446ecc79355263fd
Nancy Piedra <nancy_piedra@yahoo.com>
2020-02-25 20:20:58 +0000

    get tenant by name

--- server.js
+++ server.js
@@ -10,15 +10,20 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+const Tenant = TenantModel(sequelize, Sequelize);
 
 app.get('/', (req, res) => {
     res.send('tenant-api.phowma.com');
 });
 
 app.get('/tenants', (req, res) => {
-  const sequelize = new Sequelize(process.env.DATABASE_URL);
-  const Tenant = TenantModel(sequelize, Sequelize);
   Tenant.findAll().then(tenants => res.json(tenants))
 });
 
+app.get('/tenant', (req, res) => {
+   Tenant.findOne({where: {name: req.query.name}}).then(tenant => {
+       res.json(tenant);
+   });
+});
 app.listen(PORT, HOST);
